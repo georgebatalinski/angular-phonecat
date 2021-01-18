0406815b45911dac16da9e5e2943521554e34d78
George Batalinski <george.batalinski@gmail.com>
2021-01-18 07:56:44 -0500

    (typescript) import all the dependecies from /lib folder for now

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,17 @@
 'use strict';
 var angular = require('angular'); 
+import "./lib/jquery/dist/jquery.js";
+import "./lib/angular/angular.js";
+import "./lib/angular-animate/angular-animate.js";
+import "./lib/angular-resource/angular-resource.js";
+import "./lib/ui-router/angular-ui-router.min.js";
 // Define the `phonecatApp` module
 angular.module('phonecatApp', [
   'ngAnimate',
   'ui.router',
-  'core',
-  'phoneDetail',
-  'phoneList'
+  // 'core',
+  // 'phoneDetail',
+  // 'phoneList'
 ]);
 
 angular.element(function() {
