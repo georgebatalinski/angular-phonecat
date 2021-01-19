d04d989502769ca34fd265b11589f01351b0ccc3
George Batalinski <george.batalinski@gmail.com>
2021-01-19 13:57:40 -0500

    (ngUpgrade) add angular module, rename angular.js module, install rxjs

---
+++ app/app.module.ajs.ts
@@ -0,0 +1,45 @@
+'use strict';
+var angular = require('angular'); 
+import "./lib/jquery/dist/jquery.js";
+import "./lib/angular/angular.js";
+import "./lib/angular-animate/angular-animate.js";
+import "./lib/angular-resource/angular-resource.js";
+import "./lib/ui-router/angular-ui-router.min.js";
+//import * as configFn from "./app.config";
+import core from './core/core.module';
+import phonelist from './phone-list/phone-list.module';
+// Define the `phonecatApp` module
+
+const MODULE_NAME = 'phonecatApp';
+
+const app = angular.module(MODULE_NAME, [
+  'ngAnimate',
+  'ui.router',
+  'ngResource',
+  'core',
+  //'phoneDetail',
+  'phoneList'
+]).config(($stateProvider) => {
+  var helloState = {
+    name: 'phones',
+    url: '/phones',
+    template: '<phone-list></phone-list>'
+  }
+
+  var aboutState = {
+    name: 'phonesid',
+    url: '/phones/:phoneId',
+    template: '<phone-detail></phone-detail>'
+  }
+
+  $stateProvider.state(helloState);
+  $stateProvider.state(aboutState);
+});
+core(angular);
+phonelist(angular);
+
+angular.element(function() {
+  angular.bootstrap(document, ['phonecatApp']);
+});
+
+export default MODULE_NAME;
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,41 +1,12 @@
-'use strict';
-var angular = require('angular'); 
-import "./lib/jquery/dist/jquery.js";
-import "./lib/angular/angular.js";
-import "./lib/angular-animate/angular-animate.js";
-import "./lib/angular-resource/angular-resource.js";
-import "./lib/ui-router/angular-ui-router.min.js";
-//import * as configFn from "./app.config";
-import core from './core/core.module';
-import phonelist from './phone-list/phone-list.module';
-// Define the `phonecatApp` module
-
-const app = angular.module('phonecatApp', [
-  'ngAnimate',
-  'ui.router',
-  'ngResource',
-  'core',
-  //'phoneDetail',
-  'phoneList'
-]).config(($stateProvider) => {
-  var helloState = {
-    name: 'phones',
-    url: '/phones',
-    template: '<phone-list></phone-list>'
-  }
-
-  var aboutState = {
-    name: 'phonesid',
-    url: '/phones/:phoneId',
-    template: '<phone-detail></phone-detail>'
-  }
-
-  $stateProvider.state(helloState);
-  $stateProvider.state(aboutState);
-});
-core(angular);
-phonelist(angular);
-
-angular.element(function() {
-  angular.bootstrap(document, ['phonecatApp']);
-});
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { UpgradeModule } from '@angular/upgrade/static';
+@NgModule({
+    imports: [
+        BrowserModule,
+        UpgradeModule
+    ]
+})
+export class AppModule { 
+    constructor(private upgrade: UpgradeModule){}
+}
