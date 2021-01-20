661a85824b55eed1b58f7830f315c8c42e79e1f6
George Batalinski <george.batalinski@gmail.com>
2021-01-20 17:09:14 -0500

    (npm) add ngResource and UiRouter via imports

--- app/app.module.ajs.ts
+++ app/app.module.ajs.ts
@@ -1,11 +1,14 @@
 'use strict';
-var angular = require('angular'); 
-import "./lib/jquery/dist/jquery.js";
-import "./lib/angular/angular.js";
-import "./lib/angular-animate/angular-animate.js";
-import "./lib/angular-resource/angular-resource.js";
-import "./lib/ui-router/angular-ui-router.min.js";
+// var angular = require('angular'); 
+// import "./lib/jquery/dist/jquery.js";
+// import "./lib/angular/angular.js";
+// import "./lib/angular-animate/angular-animate.js";
+// import "./lib/angular-resource/angular-resource.js";
+// import "./lib/ui-router/angular-ui-router.min.js";
 //import * as configFn from "./app.config";
+import * as angular from "angular";
+import uiRouter from "@uirouter/angularjs";
+import ngResource from "angular-resource";
 import core from './core/core.module';
 import phonelist from './phone-list/phone-list.module';
 // Define the `phonecatApp` module
@@ -13,9 +16,9 @@ import phonelist from './phone-list/phone-list.module';
 const MODULE_NAME = 'phonecatApp';
 
 const app = angular.module(MODULE_NAME, [
-  'ngAnimate',
-  'ui.router',
-  'ngResource',
+  //'ngAnimate',
+  uiRouter,
+  require('angular-resource'),
   'core',
   //'phoneDetail',
   'phoneList'
